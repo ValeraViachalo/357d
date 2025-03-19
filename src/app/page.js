c97e80c307c876309a18938f5af926bac911db3e
ValeriiViachalo <valeraviachalo@gmail.com>
2025-03-19 12:55:15 +0200

    a

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,17 @@ import { useLanguageContent } from "@/lib/helpers/useLanguageContent";
 import { DataProvider } from "@/lib/providers/DataProvider/DataProvider";
 import WhyUs from "@/utils/WhyUs/WhyUs";
 
-export const generateMetadata = async () => generatePagesMetadata(URL_HOME);
+// export const generateMetadata = async () => generatePagesMetadata(URL_HOME);
 
 export default async function Home() {
-  const preparedData = await getFetchData(URL_HOME);
-  const data = useLanguageContent(preparedData, "en");
+  // const preparedData = await getFetchData(URL_HOME);
+  // const data = useLanguageContent(preparedData, "en");
 
   return (
-    <DataProvider data={data}>
-      <h1>{data.seo.documentTitle}</h1>
-      {/* <HomePage /> */}
-      {/* <WhyUs /> */}
-    </DataProvider>
+    // <DataProvider data={data}>
+      <h1>357</h1>
+    //   {/* <HomePage /> */}
+    //   {/* <WhyUs /> */}
+    // </DataProvider>
   );
 }
